39e63a4bcb3e405dae6c84f913bfd4e200de595a
jonas <94145174+arael34@users.noreply.github.com>
2022-11-07 11:05:14 -0500

    integrated reqwest

--- src/scripts/search.rs
+++ src/scripts/search.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, Mutex};
+use std::error::Error;
 // use std::thread;
 
 const URLS: &'static [&'static str] = &[
@@ -9,9 +10,17 @@ pub fn search(needle: &String) -> Result<(), String>{
     let data = Arc::new(
         Mutex::<Vec<String>>::new(vec![])
     );
-    for url in URLS {
+    for url in URLS { // this should eventually be computed in parallel
         let req_url = url.to_string() + needle;
-        println!("{}", req_url);
+        let resp = request(&req_url);
+
+        // if resp is not an error, add url to data maybe. or just print ...
     }
+    Ok(())
+}
+
+fn request(url: &String) -> Result<(), Box<dyn Error>> {
+    let resp = reqwest::blocking::get(url)?.text()?;
+    println!("{:#?}", resp);
     Ok(())
 }

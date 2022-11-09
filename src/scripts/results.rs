fda143b23eaa1f6d64cde609906fca21be30061d
jonas <94145174+arael34@users.noreply.github.com>
2022-11-09 17:17:39 -0500

    queryresult type

--- src/scripts/results.rs
+++ src/scripts/results.rs
@@ -6,31 +6,31 @@ pub enum Status {
 }
 
 impl Status {
-    pub fn to_string() -> String {
+    pub fn to_string(&self) -> String {
         match self {
-            Status::EXISTS => return "this username already exists.",
-            Status::DNE => return "this username is available.",
-            Status::ILLEGAL => return "this username isn't allowed.",
-            Status::ERR => return "an error occured.",
-            _ => todo!(),
+            Status::EXISTS => "this username already exists.".to_string(),
+            Status::DNE => "this username is available.".to_string(),
+            Status::ILLEGAL => "this username isn't allowed.".to_string(),
+            Status::ERR => "an error occured.".to_string(),
+            _ => "".to_string(),
         }
     }
 }
 
-pub struct Result {
-    username: String,
-    url: String,
-    user_url: String,
+pub struct QueryResult<'a> {
+    username: &'a String,
+    url: &'a String,
+    user_url: &'a String,
     status: Status,
     query_time: i32,
 }
 
-impl Result {
-    pub fn new(username: String,
-               url: String,
-               user_url: String,
+impl<'a> QueryResult<'_> {
+    pub fn new(username: &String,
+               url: &String,
+               user_url: &String,
                status: Status,
                query_time: i32) -> Self {
-        Result{ username, url, user_url, status, query_time }
+        QueryResult{ username, url, user_url, status, query_time }
     }
 }
--- src/scripts/search.rs
+++ src/scripts/search.rs
@@ -1,6 +1,8 @@
 use std::sync::{Arc, Mutex};
 // use std::thread;
 
+use crate::scripts::results::{QueryResult, Status};
+
 const URLS: &'static [&'static str] = &[
     "hi mom/",
 ];
@@ -12,12 +14,22 @@ pub async fn search(needle: &String) -> Result<(), reqwest::Error>{
     );
     for url in URLS { // this should eventually be computed in parallel
         let req_url = url.to_string() + needle;
-        let resp = client.get(req_url).send().await;
+        let resp = client.get(&req_url).send().await;
 
         if let Err(e) = resp { return Err(e) }
 
-        // data.push("")
+        let query_result = QueryResult::new(&needle, 
+                                                        &url.to_string(), 
+                                                        &req_url, 
+                                                        Status::DNE, 0);
+
+        let c_data = Arc::clone(&data);
+        let found_urls = &mut *c_data.lock().unwrap();
+        found_urls.push(req_url);
         // if resp is not an error, add url to data maybe. or just print ...
+    }
+    for url in &*data.lock().unwrap() {
+        println!("Found needle in {}", url);
     }
     Ok(())
 }
